6c7f458b065ce9f4af58dc4d6e8738f0b0266e25
misha <misha@MIKHAIL>
2026-02-13 23:20:04 +0700

    Добавлены контракты для обработки исключения с ошибками валидации

---
+++ MonitoringService.Core/Contracts/ValidationError.cs
@@ -0,0 +1,9 @@
+﻿namespace MonitoringService.Core.Contracts
+{
+	/// <summary>
+	/// Контракт для ошибки валидации.
+	/// </summary>
+	/// <param name="Field">Поле, провалившее валидацию.</param>
+	/// <param name="Message">Ошибка валидации.</param>
+	public record ValidationError(string Field, string Message);
+}
---
+++ MonitoringService.Core/Contracts/ValidationErrorResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MonitoringService.Core.Contracts
+{
+	/// <summary>
+	/// Контракт для ответа от сервера при ошибках валидации.
+	/// </summary>
+	/// <param name="Errors">Ошибки валидации.</param>
+	public record ValidationErrorResponse(IReadOnlyList<ValidationError> Errors);
+}
